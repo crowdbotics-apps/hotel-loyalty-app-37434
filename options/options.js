ec30861d206a5938e5389e2c874ad704cea3ed7c
Crowdbotics <team@crowdbotics.com>
2022-10-31 14:17:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hotel_loyalty_app_37434",
+  url: "https://hotel_loyalty_app_37434.botics.co",
+  api: "https://hotel_loyalty_app_37434.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
